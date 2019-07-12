ed785c473b0e996489f240241de90cc63a8dec95
jsukha <jsukha@users.noreply.github.com>
2019-07-11 20:20:28 -0400

    Use CMake to build test files
    Problem:
      - The project uses hard-coded makefiles to build various tests
        in the project.
      - The Makefile has hardcoded a specific set of compiler flags.
    
    Solution:
      - Use CMake to build the tests.
      - Create multiple toolchain files to handle different compiler settings.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## Usage: ./build.sh [toolchain]
+
+if [ -z $1 ]; then
+    TOOLCHAIN=clang
+    BUILD_DIR=build
+else
+    TOOLCHAIN=$1
+    BUILD_DIR=build_${TOOLCHAIN}
+fi
+echo "Using toolchain: ${TOOLCHAIN}"   
+
+rm -rf ${BUILD_DIR}
+mdkir ${BUILD_DIR}
+cmake -DCMAKE_TOOLCHAIN_FILE=toolchains/${TOOLCHAIN}.cmake -H. -B${BUILD_DIR}
+make -j -C ${BUILD_DIR}
+make -j -C ${BUILD_DIR} test
+
+
